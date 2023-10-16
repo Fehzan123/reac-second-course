da784e468b8ccf573f6f66208579958c93860b30
Fehzan123 <97243116+Fehzan123@users.noreply.github.com>
2023-10-16 13:42:14 +0530

    Styling , Debugging and Lifecyle Methods in React
    useReducer

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Counter from './Innnn';
 import Minput from './Minput';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
+     
+     
+      <Counter/>
       <Minput/>
       
     </div>
---
+++ Innnn.js
@@ -0,0 +1,35 @@
+import React, { useReducer } from 'react';
+
+// Reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+// Component using useReducer
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  const increment = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const decrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+export default Counter;
